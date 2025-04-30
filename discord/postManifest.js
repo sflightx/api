960f2cd1cdc9f7a203f2f8b06d8e8db37978bbb3
sflightx <spaceflightxploration@gmail.com>
2025-04-30 21:13:53 +0800

    fix: correct import path for Discord token and channel ID

--- discord/postManifest.js
+++ discord/postManifest.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const path = require("path");
 const { Client, GatewayIntentBits, EmbedBuilder } = require("discord.js");
-const { DISCORD_TOKEN, CHANNEL_ID } = require("https://sflightx.com/resources/serviceAuth/initializeDiscord.js");
+const { DISCORD_TOKEN, CHANNEL_ID } = require("../auth/initializeDiscord.js");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
