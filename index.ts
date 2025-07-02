ed576db1363f67e50d52979fda9630ac3d3ae3c0
sflightx <spaceflightxploration@gmail.com>
2025-07-02 22:52:23 +0800

    support also discord in index

--- index.ts
+++ index.ts
@@ -1,6 +1,11 @@
 import express from "express";
 import fcmRoutes from "./fcm/sendNotification";
 
+// Import postManifest.js (and run it immediately)
+import("./discord/postManifest.js")
+  .then(() => console.log("Discord manifest posted."))
+  .catch((err) => console.error("Failed to post Discord manifest:", err));
+
 const app = express();
 
 app.use(express.json());
